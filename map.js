c05a3890c25ed9438e4de7a3aa96d462dc16b155
HyungjuJang <dacapolife87@gmail.com>
2013-11-26 09:45:12 +0900

    googlemap Infobox
    googlemap Infobox

--- map.js
+++ map.js
@@ -2,5 +2,11 @@ $(function() {
             // Also works with: var yourStartLatLng = '59.3426606750, 18.0736160278';
             var autumnmill = new google.maps.LatLng(37.548128, 126.918691);
             $('#map_canvas').gmap({'center':autumnmill,'zoom':15});
-			      $('#map_canvas').gmap('addMarker',{'position':autumnmill});
+			$('#map_canvas').gmap('addMarker',{'position':autumnmill}).
+			click(function(){	
+				$('#map_canvas').gmap('openInfoWindow',{
+					'content':'<p>가을방아</p><img src=http://placehold.it/32x32>'
+				},this)
+			});
+			
     });

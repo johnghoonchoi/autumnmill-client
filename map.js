1b8a08bb09d66d4fd6f89b7fb0d7baaee1bc7781
HyungjuJang <dacapolife87@gmail.com>
2013-11-26 18:31:16 +0900

    polyline_map
    polyline_map

--- map.js
+++ map.js
@@ -1,6 +1,9 @@
 $(function() {
             // Also works with: var yourStartLatLng = '59.3426606750, 18.0736160278';
             var autumnmill = new google.maps.LatLng(37.548128, 126.918691);
+            var directpasth = [];
+            directpasth.push(new google.maps.LatLng(37.548128, 126.918691));  
+            directpasth.push(new google.maps.LatLng(37.54774, 126.922763)); 
             $('#map_canvas').gmap({'center':autumnmill,'zoom':15});
 			$('#map_canvas').gmap('addMarker',{'position':autumnmill}).
 			click(function(){	
@@ -8,5 +11,12 @@ $(function() {
 					'content':'<p>가을방아</p><img src=http://placehold.it/32x32>'
 				},this)
 			});
-			
-    });
+			console.log(directpasth);
+			$('#map_canvas').gmap('addShape', 'Polyline', 
+				{  'path': directpasth,
+				   'strokeColor': "#FF0000", 
+				   'strokeOpacity': 0.5, 
+				   'strokeWeight': 3, 
+				   'fillColor': "#FF0000", 
+				   'fillOpacity': 0.35 });
+});
